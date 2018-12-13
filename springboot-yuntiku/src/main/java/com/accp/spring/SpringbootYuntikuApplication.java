4e10a6d926402e2371fd24552e5ff17f52785b21
373376253 <373376253@users.noreply.github.com>
2018-12-13 13:51:30 +0800

    2

---
+++ springboot-yuntiku/src/main/java/com/accp/spring/SpringbootYuntikuApplication.java
@@ -0,0 +1,14 @@
+package com.accp.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootYuntikuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootYuntikuApplication.class, args);
+	}
+
+}
+
